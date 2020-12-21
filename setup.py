3709bd1c38d726b0d6586f2b761e182dee5048f9
fedric95 <ricciuti.federico@gmail.com>
2020-12-21 15:45:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup
 setup(
     name='WikiSyns',
     version='0.1.0',    
-    packages=['WikiSyns']
+    packages=['WikiSyns'],
+    install_requires=[
+        'sparqlwrapper>=1.8.5'
+    ]
 )
